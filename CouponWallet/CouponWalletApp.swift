5c05e567aced9864b0e78feeb621bba796b59750
Youngmin Cho <choyoungmin0322@gmail.com>
2025-03-06 09:36:42 +0900

    Initial Commit

---
+++ CouponWallet/CouponWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  CouponWalletApp.swift
+//  CouponWallet
+//
+//  Created by 조영민 on 3/6/25.
+//
+
+import SwiftUI
+
+@main
+struct CouponWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
