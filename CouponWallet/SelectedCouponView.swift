256b2a064d5b473ac8f0d0bd908496559fb59e41
aerohero <aerohero@icloud.com>
2025-03-07 18:40:58 +0900

    쿠폰선택뷰 2개로 분할

--- CouponWallet/SelectedCouponView.swift
+++ CouponWallet/SelectedCouponView.swift
@@ -9,41 +9,45 @@ import SwiftUI
 import SwiftData
 
 struct SelectedCouponView: View {
-    let selectedGifticons = Gifticon.coupons
+    let selectedCoupons = Gifticon.coupons
     @State private var selectedIndex: Int = 0
     
     var body: some View {
-        TabView(selection: $selectedIndex) {
-            ForEach(0..<selectedGifticons.count, id: \.self) { index in
-                SelectedCouponCell(selectedCoupon: selectedGifticons[index])
+        NavigationView {
+            VStack {
+                TabView(selection: $selectedIndex) {
+                    ForEach(0..<selectedCoupons.count, id: \.self) { index in
+                        SelectedCouponCell(selectedCoupon: selectedCoupons[index])
+                    }
+                }
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always)) // 페이지 스타일 적용
+                .animation(.easeInOut, value: selectedIndex) // 부드러운 애니메이션 효과
+                
+                HStack {
+                    Spacer()
+                    NavigationLink(destination: AvailableGifticonView()) {
+                        Text("사용하지 않기")
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color.gray.opacity(0.2))
+                            .cornerRadius(10)
+                    }
+                    Spacer()
+                    
+                    NavigationLink(destination: ExpiredView()) {
+                        Button("사용하기", action: {                        selectedCoupons[selectedIndex].isUsed = true
+                        })
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                        Spacer()
+                    }
+                }
+                .padding()
             }
         }
-        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always)) // 페이지 스타일 적용
-        .animation(.easeInOut, value: selectedIndex) // 부드러운 애니메이션 효과
-        
-        HStack {
-            Spacer()
-            Button("사용하지 않기") {
-                // 홈 화면으로 돌아가기
-                AvailableGifticonView()
-            }
-            .frame(maxWidth: .infinity)
-            .padding()
-            .background(Color.gray.opacity(0.2))
-            .cornerRadius(10)
-            Spacer()
-            
-            Button("사용하기") {
-                selectedGifticons[selectedIndex].isUsed = true
-            }
-            .frame(maxWidth: .infinity)
-            .padding()
-            .background(Color.blue)
-            .foregroundColor(.white)
-            .cornerRadius(10)
-            Spacer()
-        }
-        .padding()
     }
 }
             
@@ -63,10 +67,10 @@ struct SelectedCouponCell: View {
                     .font(.headline)
                 Text(selectedCoupon.brand)
                     .font(.body)
-                Text("쿠폰 사용기간 만료일: ~ \(selectedCoupon.formattedExpiryDate) 까지")
+                Text("쿠폰 만료일: ~ \(selectedCoupon.formattedExpiryDate) 까지")
                     .font(.body)
                 HStack {
-                    Image(systemName: selectedCoupon.isUsed ? "xmark.circle.fill" : "checkmark.circle.fill")
+                    Image(systemName: selectedCoupon.isUsed ? "xmark.circle.fill" : "checkmark.circle")
                     Text(selectedCoupon.isUsed ? "사용 불가" : "사용 가능")
                         .font(.body)
                 }
@@ -78,3 +82,5 @@ struct SelectedCouponCell: View {
 #Preview {
     SelectedCouponView()
 }
+
+//                    NavigationLink(destination: selectedCoupons[selectedIndex].isUsed ? ExpiredView() : AvailableGifticonView())
