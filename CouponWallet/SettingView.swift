59721a360baeedce58daa9466394ba39f310aa35
Youngmin Cho <choyoungmin0322@gmail.com>
2025-03-10 09:45:22 +0900

    SettingView 화면 텍스트 수정 및 탭바 아이콘 색상 변경

--- CouponWallet/SettingView.swift
+++ CouponWallet/SettingView.swift
@@ -12,21 +12,21 @@ struct SettingView: View {
         NavigationView {
             List {
                 Section(header: Text("계정")) {
-                    NavigationLink("프로필 설정", destination: Text("프로필 설정 화면"))
-                    NavigationLink("알림 설정", destination: Text("알림 설정 화면"))
+                    NavigationLink("프로필 설정", destination: Text("Comming soon..."))
+                    NavigationLink("알림 설정", destination: Text("이것도 Comming soon..."))
                     NavigationLink("휴지통", destination: Text("기프티콘 휴지통 화면"))
                 }
                 
                 Section(header: Text("앱 설정")) {
                     NavigationLink("테마 설정", destination: Text("테마 설정 화면"))
-                    NavigationLink("언어 설정", destination: Text("언어 설정 화면"))
-                    NavigationLink("정렬 기준", destination: Text("정렬 기준 화면"))
+                    NavigationLink("언어 설정", destination: Text("이것도 Comming soon..."))
+                    NavigationLink("정렬 기준", destination: Text("이것도 Comming soon..."))
                 }
                 
                 Section(header: Text("정보")) {
                     Text("앱 버전 1.0.0")
-                    NavigationLink("개인정보 처리방침", destination: Text("개인정보 처리방침 화면"))
-                    NavigationLink("이용약관", destination: Text("이용약관 화면"))
+                    NavigationLink("개인정보 처리방침", destination: Text("이것도 Comming soon..."))
+                    NavigationLink("이용약관", destination: Text("이것도 Comming soon..."))
                 }
             }
             .navigationTitle("설정")
